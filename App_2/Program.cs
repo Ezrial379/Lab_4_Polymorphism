df6539f7afaa83ebc9ae4d988fb91bb86a7d7ded
Ezrial379 <aaa503634@gmail.com>
2022-10-20 14:12:06 +0300

    Фіналочка

---
+++ App_2/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            ClassRoom classRoom = new ClassRoom();
+
+            ExcelentPupil excelentPupil = new ExcelentPupil();
+            GoodPupil goodPupil = new GoodPupil();
+            StandartPupil standartPupil = new StandartPupil();
+            BadPupil badPupil = new BadPupil();
+
+            classRoom.Entrie(excelentPupil);
+            classRoom.Entrie(goodPupil);
+            classRoom.Entrie(standartPupil);
+            classRoom.Entrie(badPupil);
+
+        }
+
+    }
+
+    class ClassRoom
+    {
+
+        public void Entrie (Pupil pupil)
+        {
+
+            pupil.Study();
+            pupil.Read();
+            pupil.Write();
+            pupil.Relax();
+
+        }
+
+    }
+
+    abstract class Pupil
+    {
+
+        public abstract void Study();
+
+        public abstract void Read();
+
+        public abstract void Write();
+
+        public abstract void Relax();
+  
+    }
+
+
+    class ExcelentPupil : Pupil
+    {
+
+        public override void Study()
+        {
+            Console.WriteLine("\nThe pupil studies perfectly");
+        }
+
+        public override void Read()
+        {
+            Console.WriteLine("The pupil reads perfectly");
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine("The pupil writes perfectly");
+        }
+
+        public override void Relax()
+        {
+            Console.WriteLine("The pupil relaxes well\n");
+        }
+
+    }
+
+    class GoodPupil : Pupil
+    {
+        public override void Study()
+        {
+            Console.WriteLine("\nThe pupil studies well");
+        }
+
+        public override void Read()
+        {
+            Console.WriteLine("The pupil reads well");
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine("The pupil writes well");
+        }
+
+        public override void Relax()
+        {
+            Console.WriteLine("The pupil doesn`t relax much\n");
+        }
+
+    }
+
+    class StandartPupil : Pupil
+    {
+
+        public override void Study()
+        {
+            Console.WriteLine("\nThe pupil studies normally");
+        }
+
+        public override void Read()
+        {
+            Console.WriteLine("The pupil reads well");
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine("The pupil writes normally");
+        }
+
+        public override void Relax()
+        {
+            Console.WriteLine("The pupil relaxes a lot\n");
+        }
+
+
+    }
+
+
+    class BadPupil : Pupil
+    {
+
+        public override void Study()
+        {
+            Console.WriteLine("\nThe pupil studies bad");
+        }
+
+        public override void Read()
+        {
+            Console.WriteLine("The pupil reads bad");
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine("The pupil writes bad");
+        }
+
+        public override void Relax()
+        {
+            Console.WriteLine("Mode: chilling all the time\n");
+        }
+
+
+    }
+
+
+
+
+}
---
+++ App_3/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Key;
+            Console.WriteLine($"Введiть ключ доступу (можете вiдмовитись, натиснувши символ 'N') :");
+            Key = Console.ReadLine();
+            Console.WriteLine($"Ваш ключ: '{Key}'\n");
+
+            switch (Key)
+            {
+
+                case "pro":
+                    {
+                        Console.WriteLine($"Вас вiтає версiя Pro\n");
+                        ProDocumentWorker proDocumentWorker = new ProDocumentWorker();
+                        proDocumentWorker.OpenDocument();
+                        proDocumentWorker.EditDocument();
+                        proDocumentWorker.SaveDocument();
+
+                        break;
+                    }
+
+
+                case "exp":
+                    {
+                        Console.WriteLine($"Вас вiтає версiя Expert\n");
+                        ExpertDocumentWorker expertDocumentWorker = new ExpertDocumentWorker();
+                        expertDocumentWorker.OpenDocument();
+                        expertDocumentWorker.EditDocument();
+                        expertDocumentWorker.SaveDocument();
+
+                        break;
+                    }
+
+
+                case "N":
+                    {
+                        Console.WriteLine($"Вас вiтає безкоштовна версія\n");
+                        DocumentWorker docworker = new DocumentWorker();
+                        docworker.OpenDocument();
+                        docworker.EditDocument();
+                        docworker.SaveDocument();
+
+                        break;
+                    }
+
+
+                    default:
+
+                    Console.WriteLine("Невiрна дiя. Такого коду немає.");
+
+                        break;
+
+            }
+            
+
+
+        }
+
+    }
+
+
+    class DocumentWorker
+    {
+
+        public virtual void OpenDocument()
+        {
+
+            Console.WriteLine("Документ вiдкритий");
+
+        }
+
+        public virtual void EditDocument()
+        {
+
+            Console.WriteLine("Правка документа доступна у версiї - Pro");
+
+        }
+
+        public virtual void SaveDocument()
+        {
+
+            Console.WriteLine("Збереження документа доступне у версiї - Pro");
+
+        }
+
+    }
+
+
+    class ProDocumentWorker : DocumentWorker
+    {
+
+        public override void EditDocument()
+        {
+
+            Console.WriteLine("Документ вiдредаговано");
+
+        }
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ збережено у старому форматi, збереження в iнших форматах доступне у версiї - Expert");
+        }
+
+    }
+
+
+    class ExpertDocumentWorker : ProDocumentWorker
+    {
+
+        public override void SaveDocument()
+        {
+
+            Console.WriteLine("Документ збережений в новому форматi");
+
+        }
+
+    }
+
+
+
+
+
+}
